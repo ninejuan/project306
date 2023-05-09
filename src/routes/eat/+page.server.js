1e33df084a382c6731e9259de5e3a8bf8c719fe9
주안 <juanjr123@naver.com>
2023-05-09 11:54:07 +0900

    fix: 시간표 날짜 형식 버그(일) 수정

--- src/routes/eat/+page.server.js
+++ src/routes/eat/+page.server.js
@@ -1,12 +1,11 @@
 import req from 'request'
 import env from 'dotenv'
-import setting from '../config.js'
 env.config();
 
 function getWeekDate() {
-    const x = new Date();
-    const day = x.getDay();
+    const day = new Date().getDay();
     if (day == 0 || day == 6) {
+        // 주말인 경우
         const today = new Date();
         const dayOfWeek = today.getDay();
         const daysSinceMonday = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
@@ -14,14 +13,17 @@ function getWeekDate() {
         monday.setDate(today.getDate() - daysSinceMonday);
 
         const year = monday.getFullYear();
-        let month;
-        const date = monday.getDate();
+        let month = monday.getMonth() + 1;
+        let date = monday.getDate();
 
+        // 5월을 05월로 변환
         if (monday.getMonth() < 10) {
             month = `0${monday.getMonth() + 1}`;
-        } else {
-            month = monday.getMonth() + 1;
         }
+        if (monday.getDate() < 10) {
+            date = `0${monday.getDate()}`
+        }
+        
 
         return `${year}${month}${date}`
     } else {
@@ -31,11 +33,21 @@ function getWeekDate() {
         const daysSinceMonday = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
         const monday = new Date(today);
         monday.setDate(today.getDate() - daysSinceMonday);
+
+        const year = monday.getFullYear();
+        let month = monday.getMonth() + 1;
+        let date = monday.getDate();
+
+        // 5월을 05월로 변환
         if (monday.getMonth() < 10) {
-            return `${monday.getFullYear()}0${monday.getMonth() + 1}${monday.getDate()}`
-        } else {
-            return `${monday.getFullYear()}${monday.getMonth() + 1}${monday.getDate()}`
+            month = `0${monday.getMonth() + 1}`;
         }
+        if (monday.getDate() < 10) {
+            date = `0${monday.getDate()}`
+        }
+        
+
+        return `${year}${month}${date}`
     }
 }
 
