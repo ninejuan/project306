5f106ab82a21360f9d18ba5b4f8d5323929057a7
Juan Lee <ninejuan@proton.me>
2023-05-27 00:51:34 +0900

    fix: 급식 페이지에서 Mixed Content 뜨는 문제

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 	<link rel="stylesheet" href="/css/footer.css" />
 	<link rel="stylesheet" href="/css/main.css" />
 	<link rel="stylesheet" href="/css/specific.css" />
