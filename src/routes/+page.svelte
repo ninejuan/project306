70f2ec488eb5f6f2353f5a085ef4da76a7302fc9
Juan Lee <ninejuan@proton.me>
2023-06-03 02:09:55 +0900

    fix: Mobile UI 로딩 안되는 문제

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,18 +29,16 @@
 	</header>
 	<link rel="stylesheet" href="/css/content.css" />
 	<br /><br /><br /><br />
-	<div class="commonView" />
 	<div class="pcView">
 		<br />
-		<!--PC, Tablet-->
+		<!-- PC, Tablet -->
 		<div class="mainIntro tWhite tBold fRight">
-			<h1 class="cProjectName">프로젝트 306</h1>
-			<h2 class="cProjectDescription">당신의 학교 생활에 필요한 모든 정보를 제공합니다.</h2>
+			<h1 class="cProjectName tOrange">프로젝트 306</h1>
+			<h2 class="cProjectDescription tSmoodyOrange">당신의 학교 생활에 필요한 모든 정보를 제공합니다.</h2>
 		</div>
-		<span style="line-height: 15vw;"><br /></span>
 		<div class="cMainData vLineMiddle">
 			<div class="fLeft">
-				<h3 class="tWhite">알림장</h3>
+				<h3 class="tWhite tBold">알림장</h3>
 				<hr style="width: 10vw; height: 2px; background-color:white; border: 0;" />
 				<div class="notiBox">
 					{#if typeof data.noti == 'object'}
@@ -58,7 +56,7 @@
 				</div>
 			</div>
 			<div class="fRight">
-				<h3 class="tWhite">급식</h3>
+				<h3 class="tWhite tBold">급식</h3>
 				<hr style="width: 10vw; height: 2px; background-color:white; border: 0;" />
 				<div class="mealBox">
 					{#if data.meal.status}
@@ -73,112 +71,113 @@
 			</div>
 			<span style="line-height: 10vw;"><br /></span>
 		</div>
-		<div class="mobileView">
-			<!--Mobile-->
-			<div class="mainIntro tWhite tMiddle">
-				<h1 class="mProjectName">프로젝트 306</h1>
-				<h2 class="mProjectDescription">학교 생활에 필요한 모든 것.</h2>
-			</div>
-			<span style="line-height: 5vh;"><br /></span>
-			<div class="pContent tWhite fLeft">
-				<h3 class="tLightOrange tBold">*TODAY*</h3>
-				<h5 class="tBold">* {data.str}</h5>
-				<br />
-			</div>
+		<span style="line-height: 15vw;"><br /></span>
+	</div>
+	<div class="mobileView">
+		<!--Mobile-->
+		<div class="mainIntro tWhite tMiddle">
+			<h1 class="mProjectName tOrange">프로젝트 306</h1>
+			<h2 class="mProjectDescription tSmoodyOrange">학교 생활에 필요한 모든 것.</h2>
+		</div>
+		<span style="line-height: 5vh;"><br /></span>
+		<div class="pContent tWhite fLeft">
+			<h3 class="tLightOrange tBold">*TODAY*</h3>
+			<h5 class="tBold">* {data.str}</h5>
 			<br />
-			<span style="line-height: 10vh;"><br /></span>
-			<div class="selectMenu">
-				<a id="btnFullWidth" class="btn btnL" href="/notice">
-					알림장<br />
-					<span class="badge badge-light desSpan">학급의 알림장입니다</span>
-				</a>
-				<a id="btnFullWidth" class="btn btnR" href="/schedule">
-					시간표<br />
-					<span class="badge badge-light desSpan">우리반 시간표</span>
-				</a>
-				<a id="btnFullWidth" class="btn btnL" href="/eat">
-					급식<br />
-					<span class="badge badge-light desSpan">오늘의 급식은 뭘까요?</span>
-				</a>
-				<a id="btnFullWidth" class="btn btnR" href="/call">
-					개인문의<br />
-					<span class="badge badge-light desSpan">누구세요?</span>
-				</a>
+		</div>
+		<br />
+		<span style="line-height: 10vh;"><br /></span>
+		<div class="selectMenu">
+			<a id="btnFullWidth" class="btn btnL" href="/notice">
+				알림장<br />
+				<span class="badge badge-light desSpan">학급의 알림장입니다</span>
+			</a>
+			<a id="btnFullWidth" class="btn btnR" href="/schedule">
+				시간표<br />
+				<span class="badge badge-light desSpan">우리반 시간표</span>
+			</a>
+			<a id="btnFullWidth" class="btn btnL" href="/eat">
+				급식<br />
+				<span class="badge badge-light desSpan">오늘의 급식은 뭘까요?</span>
+			</a>
+			<a id="btnFullWidth" class="btn btnR" href="/call">
+				개인문의<br />
+				<span class="badge badge-light desSpan">누구세요?</span>
+			</a>
+		</div>
+	</div>
+	<hr style="width: 85vw; height: 2px; background-color:white; margin: 0 auto; border: 0;" />
+	<footer class="site-footer" id="sf">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 col-md-3">
+					<h6 style="font-family: 'Jua', sans-serif;">정보</h6>
+					<p class="text-justify" style="color: gray;">
+						길음중학교 3학년 6반 학생들을 위한 종합 플랫폼입니다.
+					</p>
+				</div>
+
+				<div class="col-sm-12 col-md-3">
+					<h6 style="font-family: 'Jua', sans-serif;">개발</h6>
+					<a href="/" rel="noopener noreferrer">
+						<img alt="Designed by JuanLee" src="/assets/profile.png" width="150" height="150" />
+					</a>
+				</div>
+
+				<div class="col-xs-6 col-md-3">
+					<h6>주안Lab</h6>
+					<ul class="footer-links">
+						<li>
+							<a href="https://juany.kr" target="_blank" rel="noopener noreferrer"
+								>주안 포트폴리오</a
+							>
+						</li>
+						<li>
+							<a href="https://docs.aramy.net" target="_blank" rel="noopener noreferrer"
+								>주안Lab 바로가기</a
+							>
+						</li>
+						<li>
+							<a href="https://github.com/ninejuan" target="_blank" rel="noopener noreferrer"
+								>주안의 인생</a
+							>
+						</li>
+					</ul>
+				</div>
+
+				<div class="col-xs-6 col-md-3">
+					<h6>Contact</h6>
+					<ul class="footer-links">
+						<li><a href="mailto:me@juany.kr">Mail</a></li>
+						<li>
+							<a href="https://github.com/ninejuan" target="_blank" rel="noopener noreferrer"
+								>Github</a
+							>
+						</li>
+						<li>
+							<a href="https://discord.com/users/927049010072682516" rel="noopener noreferrer"
+								>Discord</a
+							>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<hr />
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-sm-6 col-xs-12">
+					<p class="copyright-text">
+						Copyright 2023 <a
+							style="color: lime; text-decoration:none;"
+							href="https://juany.kr"
+							target="_blank"
+							rel="noopener noreferrer">Juan Lee.</a
+						> All Rights Reserved.
+					</p>
+				</div>
 			</div>
 		</div>
-		<hr style="width: 85vw; height: 2px; background-color:white; margin: 0 auto; border: 0;" />
-		<footer class="site-footer" id="sf">
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-12 col-md-3">
-						<h6 style="font-family: 'Jua', sans-serif;">정보</h6>
-						<p class="text-justify" style="color: gray;">
-							길음중학교 3학년 6반 학생들을 위한 종합 플랫폼입니다.
-						</p>
-					</div>
-
-					<div class="col-sm-12 col-md-3">
-						<h6 style="font-family: 'Jua', sans-serif;">개발</h6>
-						<a rel="noopener noreferrer">
-							<img alt="Designed by JuanLee" src="/assets/profile.png" width="150" height="150" />
-						</a>
-					</div>
-
-					<div class="col-xs-6 col-md-3">
-						<h6>주안Lab</h6>
-						<ul class="footer-links">
-							<li>
-								<a href="https://juany.kr" target="_blank" rel="noopener noreferrer"
-									>주안 포트폴리오</a
-								>
-							</li>
-							<li>
-								<a href="https://docs.aramy.net" target="_blank" rel="noopener noreferrer"
-									>주안Lab 바로가기</a
-								>
-							</li>
-							<li>
-								<a href="https://github.com/ninejuan" target="_blank" rel="noopener noreferrer"
-									>주안의 인생</a
-								>
-							</li>
-						</ul>
-					</div>
-
-					<div class="col-xs-6 col-md-3">
-						<h6>Contact</h6>
-						<ul class="footer-links">
-							<li><a href="mailto:me@juany.kr">Mail</a></li>
-							<li>
-								<a href="https://github.com/ninejuan" target="_blank" rel="noopener noreferrer"
-									>Github</a
-								>
-							</li>
-							<li>
-								<a href="https://discord.com/users/927049010072682516" rel="noopener noreferrer"
-									>Discord</a
-								>
-							</li>
-						</ul>
-					</div>
-				</div>
-				<hr />
-			</div>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-8 col-sm-6 col-xs-12">
-						<p class="copyright-text">
-							Copyright 2023 <a
-								style="color: lime; text-decoration:none;"
-								href="https://juany.kr"
-								target="_blank"
-								rel="noopener noreferrer">Juan Lee.</a
-							> All Rights Reserved.
-						</p>
-					</div>
-				</div>
-			</div>
-		</footer>
-	</div></body
->
+	</footer>
+</body>
 <slot />
